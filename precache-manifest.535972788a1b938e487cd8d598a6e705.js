a1f15178f42a13c7a7488f3f2f481a77e1e1ed8c
Artak Mikayelyan <miqart@mail.ru>
2020-01-09 18:31:19 +0400

    Updates

---
+++ precache-manifest.535972788a1b938e487cd8d598a6e705.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9078d407659e2269927bd82aa4d8fee7",
+    "url": "/room-reservation/index.html"
+  },
+  {
+    "revision": "c5af35f70fa0a675ca13",
+    "url": "/room-reservation/static/css/main.2da5b4cb.chunk.css"
+  },
+  {
+    "revision": "0874288dcc3eccc22343",
+    "url": "/room-reservation/static/js/2.acb797ff.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/room-reservation/static/js/2.acb797ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5af35f70fa0a675ca13",
+    "url": "/room-reservation/static/js/main.2498b6f9.chunk.js"
+  },
+  {
+    "revision": "adbb8215d5efa9f6d156",
+    "url": "/room-reservation/static/js/runtime-main.9ac6883b.js"
+  },
+  {
+    "revision": "dafeafc3a99e8d5e7ae6b0c89f2c662f",
+    "url": "/room-reservation/static/media/bg.dafeafc3.svg"
+  },
+  {
+    "revision": "7b0dc7ee756a67b003dafecc0694f92d",
+    "url": "/room-reservation/static/media/edit.7b0dc7ee.svg"
+  },
+  {
+    "revision": "90e83534d686d4a3773c914979efbdab",
+    "url": "/room-reservation/static/media/loading.90e83534.svg"
+  },
+  {
+    "revision": "740028d44a1c1c3d31566dd7e2702d56",
+    "url": "/room-reservation/static/media/remove.740028d4.svg"
+  }
+]);
