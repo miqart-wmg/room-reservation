7baf5e17b55c3051468b985562ad87bf957b88ab
Artak Mikayelyan <miqart@mail.ru>
2020-11-23 17:36:49 +0400

    booking started

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import Nav from './components/Nav/Nav';
 import Rooms from './pages/Rooms/Rooms';
 import Visitors from './pages/Visitors/Visitors';
 import Bookings from './pages/Bookings/Bookings';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Router>
+    <Router basename="/room-reservation">
       <div className="App">
         <Nav />
         <Switch>
-          <Route path="/" exact component={Rooms} />
+          <Route exact path="/">
+            <Redirect to="/rooms" />
+          </Route>
           <Route path="/rooms" component={Rooms} />
           <Route path="/visitors" component={Visitors} />
           <Route path="/bookings" component={Bookings} />
+          <Redirect to="/" />
         </Switch>
       </div>
     </Router>
